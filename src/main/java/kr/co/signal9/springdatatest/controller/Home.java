335d1e31989b39399886897813f4311ac56a5be0
dnt-walker <gjtroql@gmail.com>
2023-08-20 01:06:26 +0900

    SpringBoot WEB Starter 추가 웹 관련 설정 추가 기본 샘플코드 추가.

---
+++ src/main/java/kr/co/signal9/springdatatest/controller/Home.java
@@ -0,0 +1,13 @@
+package kr.co.signal9.springdatatest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
