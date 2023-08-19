871edcf0c32eb4329adfcb9c46fb8f29409b4b21
dnt-walker <gjtroql@gmail.com>
2023-08-20 00:32:03 +0900

    First commit

---
+++ src/main/java/kr/co/signal9/springdatatest/SpringdatatestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.signal9.springdatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/signal9/springdatatest/SpringdatatestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.signal9.springdatatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
